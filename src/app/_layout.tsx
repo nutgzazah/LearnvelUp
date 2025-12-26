5959a84e8d5e7a5e0b3d74ed4d2c13beaf139f40
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2025-12-26 07:11:23 +0700

    Added Theme Color

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,9 @@
-import { Stack } from 'expo-router';
-// 👇 สำคัญมาก! ต้อง Import ไฟล์นี้เพื่อให้ Tailwind ทำงาน
+import { Stack } from "expo-router";
 import "../../global.css";
 
 export default function RootLayout() {
   return (
     <Stack>
-      {/* บอกให้ Stack โหลดหน้า (tabs) มาแสดง และซ่อนหัวข้อข้างบน */}
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary))",
+        secondary: "rgb(var(--color-secondary))",
+        background: "rgb(var(--color-background))",
+        text: "rgb(var(--color-text))",
+        card: "rgb(var(--color-card))",
+      },
+      boxShadow: {
+        custom: "0px 0px 5px 0px rgb(var(--shadow-color) / 0.5)",
+      },
+    },
   },
   plugins: [],
-}
+};
