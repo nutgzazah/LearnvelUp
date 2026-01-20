cfc5ccff1baea3bdcdfbe578a38b65f96da33168
phuparu <100335395+phuparu@users.noreply.github.com>
2026-01-20 18:41:29 +0700

    Add learn, missions, and search tab screens; update profile
    Introduces new Learn, Missions, and Search screens under the tabs directory. Updates the Profile screen to include a link to the Design System, and moves the Design System screen out of the tabs directory.

---
+++ src/app/(protected)/(tabs)/missions.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const MissionScreen = () => {
+  return (
+    <View>
+      <Text>MissionScreen</Text>
+    </View>
+  );
+};
+
+export default MissionScreen;
