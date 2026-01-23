c77687693bce4f34918962523e54aa007c96a7b8
phuparu <100335395+phuparu@users.noreply.github.com>
2026-01-23 19:24:33 +0700

    Update [id].tsx

--- src/app/(protected)/missionReward/[id].tsx
+++ src/app/(protected)/missionReward/[id].tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import RewardPage from "../../../components/RewardPage";
 
 const MissionRewardScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <RewardPage />
-    </View>
+    </SafeAreaView>
   );
 };
 
