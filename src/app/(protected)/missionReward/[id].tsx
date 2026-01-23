436341fd6b10b2a39b03423fa314d6f83615631a
phuparu <100335395+phuparu@users.noreply.github.com>
2026-01-23 16:33:31 +0700

    Organize routing missions tab and add mission reward screen
    Renamed the 'missions' tab to 'mission' and moved its main screen to a new directory structure. Added a nested layout for the mission tab and introduced a new dynamic mission reward routing screen accessible via '/missionReward/[id]'. Updated navigation and asset paths accordingly.

---
+++ src/app/(protected)/missionReward/[id].tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import RewardPage from "../../../components/RewardPage";
+
+const RewardScreen = () => {
+  return (
+    <View>
+      <RewardPage />
+    </View>
+  );
+};
+
+export default RewardScreen;
