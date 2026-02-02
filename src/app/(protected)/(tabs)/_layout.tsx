ea6253a95c2891932cc304229dd2304ee83c6646
phuparu <100335395+phuparu@users.noreply.github.com>
2026-02-03 02:38:55 +0700

    Update _layout.tsx
    Using custom header

--- src/app/(protected)/(tabs)/_layout.tsx
+++ src/app/(protected)/(tabs)/_layout.tsx
@@ -1,87 +1,103 @@
+import AppHeader from "@/src/components/AppHeader";
 import { CustomTabBar } from "@/src/components/CustomTabBar";
 import { Tabs } from "expo-router";
-import React from "react";
-import { Image } from "react-native";
+import { Image, View } from "react-native";
 
 export default function TabLayout() {
-  const homeIcon = require("../../../../assets/images/nav/home-icon.png");
-  const homeBoldIcon = require("../../../../assets/images/nav/home-icon-bold.png");
-  const searchIcon = require("../../../../assets/images/nav/search-icon.png");
-  const searchBoldIcon = require("../../../../assets/images/nav/search-icon-bold.png");
-  const learnIcon = require("../../../../assets/images/nav/learn-icon.png");
-  const learnBoldIcon = require("../../../../assets/images/nav/learn-icon-bold.png");
-  const missionIcon = require("../../../../assets/images/nav/mission-icon.png");
-  const missionBoldIcon = require("../../../../assets/images/nav/mission-icon-bold.png");
-  const profileIcon = require("../../../../assets/images/nav/profile-icon.png");
-  const profileBoldIcon = require("../../../../assets/images/nav/profile-icon-bold.png");
+  const icons = {
+    home: {
+      normal: require("../../../../assets/images/nav/home-icon.png"),
+      bold: require("../../../../assets/images/nav/home-icon-bold.png"),
+    },
+    search: {
+      normal: require("../../../../assets/images/nav/search-icon.png"),
+      bold: require("../../../../assets/images/nav/search-icon-bold.png"),
+    },
+    learn: {
+      normal: require("../../../../assets/images/nav/learn-icon.png"),
+      bold: require("../../../../assets/images/nav/learn-icon-bold.png"),
+    },
+    mission: {
+      normal: require("../../../../assets/images/nav/mission-icon.png"),
+      bold: require("../../../../assets/images/nav/mission-icon-bold.png"),
+    },
+    profile: {
+      normal: require("../../../../assets/images/nav/profile-icon.png"),
+      bold: require("../../../../assets/images/nav/profile-icon-bold.png"),
+    },
+  };
 
   return (
-    <Tabs tabBar={(props) => <CustomTabBar {...props} />}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "หน้าหลัก",
-          headerShown: true,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              className="w-7 h-7"
-              source={focused ? homeBoldIcon : homeIcon}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: "ค้นหา",
-          headerShown: true,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              className="w-7 h-7"
-              source={focused ? searchBoldIcon : searchIcon}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="learn"
-        options={{
-          title: "เรียนรู้",
-          headerShown: true,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              className="w-7 h-7"
-              source={focused ? learnBoldIcon : learnIcon}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="mission"
-        options={{
-          title: "ภารกิจ",
-          headerShown: true,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              className="w-7 h-7"
-              source={focused ? missionBoldIcon : missionIcon}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "โปรไฟล์",
-          headerShown: true,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              className="w-7 h-7"
-              source={focused ? profileBoldIcon : profileIcon}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+    <View style={{ flex: 1 }}>
+      {/* Custom Header */}
+      <AppHeader />
+
+      {/* Custom TabBar */}
+      <Tabs
+        screenOptions={{ headerShown: false }}
+        tabBar={(props) => <CustomTabBar {...props} />}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "หน้าหลัก",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                className="w-7 h-7"
+                source={focused ? icons.home.bold : icons.home.normal}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: "ค้นหา",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                className="w-7 h-7"
+                source={focused ? icons.search.bold : icons.search.normal}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="learn"
+          options={{
+            title: "เรียนรู้",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                className="w-7 h-7"
+                source={focused ? icons.learn.bold : icons.learn.normal}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="mission"
+          options={{
+            title: "ภารกิจ",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                className="w-7 h-7"
+                source={focused ? icons.mission.bold : icons.mission.normal}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "โปรไฟล์",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                className="w-7 h-7"
+                source={focused ? icons.profile.bold : icons.profile.normal}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
   );
 }
