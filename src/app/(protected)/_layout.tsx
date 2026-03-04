76457c8f16c6c25a77bec380b9c3fcfe07e850ba
phuparu <100335395+phuparu@users.noreply.github.com>
2026-03-04 18:43:06 +0700

    Update _layout.tsx
    Update profile route

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -102,9 +102,75 @@ export default function ProtectedLayout() {
           headerBackground() {
             return <View className="bg-background absolute inset-0" />;
           },
-          /* headerStyle: {
-            backgroundColor: "rgb(var(--color-background) / <alpha-value>)",
-          }, */
+          headerTitleStyle: {
+            fontSize: 19,
+            color: "rgb(var(--color-text) / <alpha-value>)",
+            fontFamily: "K2D-Regular",
+          },
+        })}
+      />
+      <Stack.Screen
+        name="profile/wishlist"
+        options={({ navigation }) => ({
+          title: "คอร์สที่อยากได้",
+          headerLeft: () => (
+            <View className="items-center">
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Image
+                  source={AppIcons.HEADERS.NORMAL.BACK[theme]}
+                  className="w-7 h-7"
+                />
+              </TouchableOpacity>
+            </View>
+          ),
+          headerRight: () => (
+            <View className="flex-row items-center px-1">
+              <Image
+                source={AppIcons.HEADERS.NORMAL.COIN}
+                className="w-5 h-5 mx-1"
+              />
+              <Text className="text-small font-bold text-text">0</Text>
+            </View>
+          ),
+          headerTitleAlign: "left",
+          headerShadowVisible: true,
+          headerSearchBarOptions: {
+            placeholder: "ค้นหาคอร์สที่อยากได้",
+            onChangeText: (text) => console.log("Search"),
+          },
+          headerBackground() {
+            return <View className="bg-background absolute inset-0" />;
+          },
+          headerTitleStyle: {
+            fontSize: 19,
+            color: "rgb(var(--color-text) / <alpha-value>)",
+            fontFamily: "K2D-Regular",
+          },
+        })}
+      />
+      <Stack.Screen
+        name="profile/achieve"
+        options={({ navigation }) => ({
+          title: "เหรียญตราความสำเร็จ",
+          headerLeft: () => (
+            <View className="items-center">
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Image
+                  source={AppIcons.HEADERS.NORMAL.BACK[theme]}
+                  className="w-7 h-7"
+                />
+              </TouchableOpacity>
+            </View>
+          ),
+          headerTitleAlign: "left",
+          headerShadowVisible: true,
+          headerSearchBarOptions: {
+            placeholder: "ค้นหาเหรียญตราความสำเร็จ",
+            onChangeText: (text) => console.log("Search"),
+          },
+          headerBackground() {
+            return <View className="bg-background absolute inset-0" />;
+          },
           headerTitleStyle: {
             fontSize: 19,
             color: "rgb(var(--color-text) / <alpha-value>)",
