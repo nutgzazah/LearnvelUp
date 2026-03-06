125add7f566742b3560c544cac525bf0c49be8a1
phuparu <100335395+phuparu@users.noreply.github.com>
2026-03-06 16:20:17 +0700

    Update _layout.tsx

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -178,6 +178,55 @@ export default function ProtectedLayout() {
           },
         })}
       />
+      <Stack.Screen
+        name="profile/achieveReward/[id]"
+        options={{
+          presentation: "card", // หรือ "modal" ทำให้เด้งขึ้นมาจากข้างล่าง (iOS style)
+          headerShown: false,
+          title: "รับรางวัล",
+        }}
+      />
+      <Stack.Screen
+        name="profile/editProfile"
+        options={({ navigation }) => ({
+          title: "แก้ไขโปรไฟล์",
+          headerLeft: () => (
+            <View className="items-center">
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Image
+                  source={AppIcons.HEADERS.NORMAL.BACK[theme]}
+                  className="w-7 h-7"
+                />
+              </TouchableOpacity>
+            </View>
+          ),
+          headerRight: () => (
+            <View className="flex-row items-center px-1">
+              <Image
+                source={AppIcons.HEADERS.NORMAL.COIN}
+                className="w-5 h-5 mx-1"
+              />
+              <Text className="text-small font-bold text-text">0</Text>
+
+              <Image
+                source={AppIcons.HEADERS.NORMAL.STREAK}
+                className="w-5 h-5 mx-1 ml-2"
+              />
+              <Text className="text-small font-bold text-text">0</Text>
+            </View>
+          ),
+          headerTitleAlign: "left",
+          headerShadowVisible: true,
+          headerBackground() {
+            return <View className="bg-background absolute inset-0" />;
+          },
+          headerTitleStyle: {
+            fontSize: 19,
+            color: "rgb(var(--color-text) / <alpha-value>)",
+            fontFamily: "K2D-Regular",
+          },
+        })}
+      />
     </Stack>
   );
 }
